add0944f8f387e7058d4c2d97c6c6d21dc3ff732
Michael <MichaelMerrittFarrell@gmail.com>
2016-05-17 14:29:18 -0400

    Updated project template package.json, babelrc, and eslint file. Updated server.js and removed unused code.

--- project_template/server.js
+++ project_template/server.js
@@ -3,7 +3,6 @@ var path = require('path');
 var webpack = require('webpack');
 var app = express();
 
-var isDevelopment = false; //(process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, "");
 
 app.use(express.static(static_path))
@@ -11,20 +10,12 @@ app.use(express.static(static_path))
   res.sendFile('index.html', {
     root: static_path
   });
-}).listen(process.env.PORT || 8080, function (err) {
+})
+
+var server = app.listen(process.env.PORT || 8080, function (err) {
   if (err) { console.log(err) };
-  console.log('Listening at localhost:8080');
+  var port = server.address().port;
+  console.log('Listening at localhost:' + port);
 });
 
-if (isDevelopment) {
-  var config = require('./webpack.config');
-  var WebpackDevServer = require('webpack-dev-server');
-  
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true
-  }).listen(3000, 'localhost', function (err, result) {
-    if (err) { console.log(err) }
-    console.log('Listening at localhost:3000');
-  });
-}
+module.exports = server;
