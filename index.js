6334e5b2ffc68d00fa7f6df1e01873336e5157c6
Michael <MichaelMerrittFarrell@gmail.com>
2016-05-23 23:53:32 -0400

    Fixed path generation bug

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function collect(val, memo) {
   return memo;
 }
 program
-  .version('1.1.4')
+  .version('1.1.5')
   .option('new <projectName>', 'Generate New React-Redux Project.')
   .option('g_container <containerName>', 'Generate Container file.')
   .option('g_component <componentName>', 'Generate Component file.')
