5bde6ef836980e515d0c401e4368efdac34ad7f1
mikeysax <michaelmerrittfarrell@gmail.com>
2016-06-11 15:36:46 -0400

    Added localStorage and jsdom global to test helpers to fit different types of testing better

--- project_template/tests/testHelpers.js
+++ project_template/tests/testHelpers.js
@@ -1,19 +1,27 @@
+import jsdomGlobal from 'jsdom-global';
 import jsdom from 'jsdom';
+import localStorage from 'localStorage';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 import chaiEnzyme from 'chai-enzyme';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
-
+win.localStorage = localStorage
 global.document = doc;
 global.window = win;
+jsdomGlobal()
+propagateToGlobal(win)
 
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
+
+function propagateToGlobal (window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue
+    if (key in global) continue
+
+    global[key] = window[key]
   }
-});
+}
 
 chai.use(chaiImmutable);
 chai.use(chaiEnzyme());
