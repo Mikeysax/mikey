aa747c358d8cd16139f44aa76f3000b1ea5a2358
mikeysax <michaelmerrittfarrell@gmail.com>
2017-03-01 09:19:14 -0500

    Fixed isomorphic config.

--- universal_api_project_template/env.js
+++ universal_api_project_template/env.js
@@ -1,3 +1,4 @@
+// Add to .gitignore
 // process.env['APP_SECRET'] = 'SecretKeysGoHere';
 // process.env['AWS_ACCESS_KEY'] = "XXXXXXXXXXXXXXX";
 // process.env['AWS_SECRET_KEY'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
