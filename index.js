ea5c5dd3769f86779d1cfdbd83070e3b1ee0fa88
mikeysax <michaelmerrittfarrell@gmail.com>
2017-03-01 09:24:05 -0500

    Fixed universal api config.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function collect(val, memo) {
 }
 
 program
-  .version('3.8.1')
+  .version('3.8.2')
   .option('new <projectName>', 'Generate New Mikey Project: react/redux/universal/electron', /^(react|redux|universal|electron)$/i)
   .option('g <fileType>', 'Generate New React File: container/component/action/reducer/helper/custom', /^(container|component|action|reducer|helper|custom)$/i)
   .option('-i, import [importName]', '(Optional) Add imports on file generation.', collect, [])
